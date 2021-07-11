da15ed011e038a788c1c3cbdc4138eea831249c0
SmallestIQ <81588751+Small1337@users.noreply.github.com>
2021-07-11 01:50:36 -0400

    sus

---
+++ kinda sus.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#define ඞ int
+#define ඞඞ main
+#define ඞඞඞ printf
+#define ඞඞඞඞ "Actova Among Us Detections V1.0"
+#define ඞඞඞඞඞඞඞඞ "----------------------------------"
+#define ඞඞඞඞඞඞඞඞඞඞඞඞ "----------------------------------\n"
+#define ඞඞඞඞඞ getchar
+#define ඞඞඞඞඞඞ system
+#define ඞඞඞඞඞඞඞ "title Actova"
+#define ඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞ system
+#define ඞඞඞඞඞඞඞඞඞඞඞඞඞ "cd C:\\Program Files\\Epic Games\\AmongUs"
+#define ඞඞඞඞඞඞඞඞඞඞ "Epic Games Among Us Detected\n"
+#define ඞඞඞඞඞඞඞඞඞඞඞ "Epic Games Among Us Not Found\n"
+#define ඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞ "Steam Among Us Detected\n"
+#define ඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞ "Steam Among Us Not Found\n"
+#define ඞඞඞඞඞඞඞඞඞ "C:\\Program Files\\Epic Games\\AmongUs\\Among Us.exe"
+#define ඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞ "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Among Us\\Among Us.exe"
+#define ඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞ std::filesystem::exists
+#define ඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞ if 
+#define ඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞ else
